d7e128fb08c6d4e32ac4a10e6ba99a5c79bf6f48
Rebecca Leon <rebecca.leonx@gmail.com>
2018-08-11 23:43:40 -0400

    Burgers App

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado Crunch Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", true);
